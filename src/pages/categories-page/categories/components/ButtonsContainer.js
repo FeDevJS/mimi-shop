2410423e69782f57cf6e5884f955072e901b513f
Fede <f.prog@outlook.com>
2022-06-18 15:24:02 -0300

    Buttons now redirect to Shop-cart-page + adds new product to cart

--- src/pages/categories-page/categories/components/ButtonsContainer.js
+++ src/pages/categories-page/categories/components/ButtonsContainer.js
@@ -1,36 +1,39 @@
 import React, { useContext, useReducer } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './buttonsContainer.css';
 import { ShopCartContext } from '../../../../contexts/ShopCartProvider';
 import { counterReducer } from '../../../../reducers/counterReducer';
 import { Button } from './Button';
 
 export const ButtonsContainer = ({ id, category, products }) => {
-	const [text, dispatch] = useReducer(counterReducer, false);
+	const [text, dispatch] = useReducer(counterReducer, 'AÑADIR');
 	const { cartData, setCartData } = useContext(ShopCartContext);
+	const navigate = useNavigate();
 	const handleAdd = () => dispatch({ 
 		action: 'add', 
 		category,
 		id,
 		cartData,
 		setCartData,
-		products
+		products,
+		navigate
 	});
-	const handleRemove = () => dispatch({ 
-		action: 'remove', 
-		category,
-		id,
-		cartData,
-		setCartData,
-		products
-	});
+	// const handleRemove = () => dispatch({ 
+	// 	action: 'remove', 
+	// 	category,
+	// 	id,
+	// 	cartData,
+	// 	setCartData,
+	// 	products
+	// });
 	return(
 		<section className='product-buttons-container'>
 			<Button 
 				btnClass={'plus-btn'} 
 				id={id} 
 				category={category} 
-				innerText={!text ? 'AGREGAR' : 'REMOVER'}
-				onClickFn={!text ? handleAdd : handleRemove}
+				innerText={text}
+				onClickFn={handleAdd}
 				/>
 		</section>
 	);
