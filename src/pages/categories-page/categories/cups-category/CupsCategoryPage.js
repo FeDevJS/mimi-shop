b47e63c32987b2a73de205143a73a0b85c936d50
Fede <f.prog@outlook.com>
2022-06-26 19:08:39 -0300

    restarting shop-cart-page & cups-category-page

--- src/pages/categories-page/categories/cups-category/CupsCategoryPage.js
+++ src/pages/categories-page/categories/cups-category/CupsCategoryPage.js
@@ -1,34 +1,12 @@
 import React from 'react';
-import { useFetch } from '../../../../hooks/useFetch';
 import { useMount } from '../../../../hooks/useMount';
-import { Loader } from '../../../../main-components/Loader';
-import { CategoryHeader } from '../components/CategoryHeader';
-import { ProductCard } from '../components/ProductCard';
 import './cups-category-page.css';
 
 const CupsCategoryPage = () => {
-	const data = useFetch("http://localhost:5000/api/products/tazas");
 	const self = useMount();
 	return(
 		<section ref={self} className='sections cups-category-section'>
-			<CategoryHeader data={data} />
-			<div className='categories-products-wrapper'>
-				{data.response !== null
-					? data.response.map((product) => (
-						<ProductCard 
-							key={product.id} 
-							productName={product.name} 
-							price={product.price} 
-							id={product.id}
-							category={product.category}
-							products={data.response}
-						/>
-					))
-					: data.error !== null
-						? <p>{data.error}</p>
-						: <Loader />
-				}
-			</div>
+			Nothing to show...
 		</section>
 		);
 };
--- src/pages/shop-cart-page/ShopCartPage.js
+++ src/pages/shop-cart-page/ShopCartPage.js
@@ -1,30 +1,17 @@
-import React, { useContext } from 'react';
+// import React, { useContext } from 'react';
 import './shop-cart-page.css';
-import { ShopCartContext } from '../../contexts/ShopCartProvider';
+// import { ShopCartContext } from '../../contexts/ShopCartProvider';
 import { useMount } from '../../hooks/useMount';
 import './shop-cart-page.css';
-import { CartProduct } from './components/CartProduct';
 import { NoProducts } from './components/NoProducts';
 
 const ShopCartPage = () => {
 	const self = useMount();
-	const { cartData } = useContext(ShopCartContext);
+	// const { cartData } = useContext(ShopCartContext);
 	return(
 		<>
 			<section ref={self} className='sections shop-cart-page-section'>
-				<h3 className='cart-header'>Carrito</h3>
-				{cartData.cartProducts.length > 0
-					? cartData.cartProducts.map(prod => (
-						<CartProduct 
-							key={prod.id}
-							id={prod.id}
-							name={prod.name}
-							quantity={prod.quantity}
-							price={prod.price}
-						/>
-					))
-					: <NoProducts />
-				}
+				<NoProducts />
 			</section>
 		</>
 	);
